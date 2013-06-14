6c3dcf1d67fd13cae99c31e0e6790d8f49ebb562
Clint Bellanger <clintbellanger@gmail.com>
2013-06-14 09:13:57 -0500

    Fedora proportions tweak

---
+++ release/js/pickups.js
@@ -0,0 +1,12 @@
+/**
+ Item pickups
+ */
+ 
+var pickups = new Object();
+pickups.list = new Array();
+pickups.img_id = 0;
+
+function pickups_init() {
+  pickups.img_id = imageset_load("images/upvote.png");
+}
+
