4f533a3519f68e6b8406358d48b3a8ba5e42fa73
clintbellanger <clintbellanger@gmail.com>
2015-07-08 10:02:12 -0500

    Fix some quotes layout

--- release/js/quotes.js
+++ release/js/quotes.js
@@ -6,7 +6,22 @@ quotes = [
   ["This sub should be banned", "You people are why we", "can't have nice things."],
   ["If you don't like it,", "get off the Internet.", ""],
   ["I mean, I'm not even mad,", "I just pity you guys.", ""],
-  ["Keep on sending", "those emails!", ""],
-  ["But what about the men?", "", ""]
+  ["Keep on sending those", "emails!", ""],
+  ["But what about the men?", "", ""],
+  ["For too long we have", "sat back and watched you", "shrill harpies propagate."],
+  ["Fuck off back to the", "hug box that is Tumblr.", "leave our site alone."],
+  ["Whenevever I visit this", "subreddit I'm left hating", "myself for having a penis."],
+  ["This place is a cesspool", "of unnecessary anger", "and self pity."],
+  ["Do you think you're", "shocking someone with", "your phallic emoticons?"],
+  ["The vast majority of the", "things submitted to this", "sub are harmless jokes."],
+  ["I won't inundate you with", "spam, though I very", "easily could."],
+  ["I'm just being a human", "instead of trying to", "seem morally superior."],
+  ["All you do is disrupt", "this unity and make", "people uncomfortable."],
+  ["If there's anything Reddit", "admins love, it's helping", "kill free speech."],
+  ["Downvote all you want,", "Shitlords will unleash a", "torrent of hatred."],
+  ["You can't cut back on our", "first amendment rights!", "you'll regret this!"],
+  ["Honestly I tell my", "girlfriend to make me", "a sammich all the time."],
+  ["You're corrupting", "everything Reddit stands", "for."],
+  ["If the world was up to", "you we'd live in some", "misandrist dictatorship."],
+  ["I will take action against", "this sub for violating", "multiple federal laws."]
 ];
-
